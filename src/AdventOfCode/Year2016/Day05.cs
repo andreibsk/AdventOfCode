c89a9ef1e2325de49e87661265854e79692ac920
Andrei Bîscă <andreibsk+github@gmail.com>
2019-11-02 10:59:02 +0200

    Add 2016.05 puzzle

---
+++ src/AdventOfCode/Year2016/Day05.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode.Year2016
+{
+	public class Day05 : Puzzle
+	{
+		public override DateTime Date => new DateTime(2016, 12, 5);
+		public override string Title => "How About a Nice Game of Chess?";
+
+		private string _doorId;
+
+		public override string CalculateSolution()
+		{
+			using var md5 = MD5.Create();
+
+			Solution = GenerateChars(md5, 6)
+				.Select(t => t.Char)
+				.Take(8)
+				.Aggregate(string.Empty, (s, c) => s + c);
+
+			return Solution;
+		}
+
+		public override string CalculateSolutionPartTwo()
+		{
+			using var md5 = MD5.Create();
+			char?[] pass = new char?[8];
+
+			foreach ((int i, char c) in GenerateChars(md5, 7))
+			{
+				if (i >= pass.Length)
+					continue;
+
+				pass[i] ??= c;
+
+				if (pass.All(c => c.HasValue))
+					break;
+			}
+
+			Solution = pass.Aggregate(string.Empty, (s, c) => s + c);
+			return Solution;
+		}
+
+		protected override void ParseInput(string[] input)
+		{
+			_doorId = input[0];
+		}
+
+		private IEnumerable<(int Index, char Char)> GenerateChars(MD5 md5, int charPosition)
+		{
+			int hexMask = charPosition % 2 == 0 ? 0x0F : 0xF0;
+			int bindex = (charPosition - 1) / 2;
+
+			return Enumerable
+				.Range(0, int.MaxValue)
+				.Select(i => _doorId + i)
+				.Select(s => Encoding.UTF8.GetBytes(s))
+				.Select(b => md5.ComputeHash(b))
+				.Where(b => b[0] == 0 && b[1] == 0 && (b[2] & 0xF0) == 0)
+				.Select(b => (b[2] & 0x0F, (b[bindex] & hexMask).ToString("x1")[0]));
+		}
+	}
+}
