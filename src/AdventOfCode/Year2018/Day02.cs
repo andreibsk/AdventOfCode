aa732b91564eef1c2dcf95442bf88171e945a6cc
Andrei Bîscă <andreibsk+github@gmail.com>
2019-11-04 20:19:07 +0200

    Add 2018.02 puzzle

---
+++ src/AdventOfCode/Year2018/Day02.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace AdventOfCode.Year2018
+{
+	public class Day02 : Puzzle
+	{
+		private readonly string[] _boxIds;
+
+		public Day02(string[] input) : base(input)
+		{
+			_boxIds = input;
+		}
+
+		public override DateTime Date => new DateTime(2018, 12, 02);
+		public override string Title => "Inventory Management System";
+
+		public override string? CalculateSolution()
+		{
+			int twos = _boxIds.Select(id => id.GroupBy(c => c)).Count(id => id.Any(g => g.Count() == 2));
+			int threes = _boxIds.Select(id => id.GroupBy(c => c)).Count(id => id.Any(g => g.Count() == 3));
+
+			return Solution = (twos * threes).ToString();
+		}
+
+		public override string? CalculateSolutionPartTwo()
+		{
+			var ids = new SortedSet<string>(new DiffByOneComparer());
+
+			foreach (string id1 in _boxIds)
+				if (ids.TryGetValue(id1, out string? id2))
+				{
+					return SolutionPartTwo = string.Concat(id1.Where((c, i) => c == id2[i]));
+				}
+				else
+					ids.Add(id1);
+
+			return SolutionPartTwo;
+		}
+
+		private class DiffByOneComparer : IComparer<string>
+		{
+			public int Compare([AllowNull] string x, [AllowNull] string y)
+			{
+				if (x == null && y == null)
+					return 0;
+
+				int d = (x?.Length ?? -1) - (y?.Length ?? -1);
+				if (d != 0)
+					return d;
+
+				bool byOne = false;
+				for (int i = 0; i < x!.Length; i++)
+				{
+					if (x[i] != y![i])
+						if (byOne)
+							return x[i] - y[i];
+						else
+							byOne = true;
+				}
+
+				return 0;
+			}
+		}
+	}
+}
---
+++ tests/AdventOfCode.Tests/Year2018/Day02Tests.cs
@@ -0,0 +1,35 @@
+using AdventOfCode.Year2018;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Tests.Year2018
+{
+	[TestClass]
+	public class Day02Tests : PuzzleTests<Day02>
+	{
+		[TestMethod]
+		[DataRow(new[]
+		{
+			"abcdef",
+			"bababc",
+			"abbcde",
+			"abcccd",
+			"aabcdd",
+			"abcdee",
+			"ababab",
+		}, "12")]
+		public void PartOneExamples(string[] input, string expected) => CalculatePartOne(input, expected);
+
+		[TestMethod]
+		[DataRow(new[]
+		{
+			"abcde",
+			"fghij",
+			"klmno",
+			"pqrst",
+			"fguij",
+			"axcye",
+			"wvxyz"
+		}, "fgij")]
+		public void PartTwoExamples(string[] input, string expected) => CalculatePartTwo(input, expected);
+	}
+}
