deb9f25a117ed72e82bbbc1b28b4cc639e5f5c0f
Andrei Bîscă <andreibsk+github@gmail.com>
2019-11-10 16:31:05 +0200

    Add 2017.19 puzzle and input

---
+++ src/AdventOfCode/Year2017/Day19.cs
@@ -0,0 +1,69 @@
+using System;
+using AdventOfCode.Common;
+using AdventOfCode.Common.Extensions;
+
+namespace AdventOfCode.Year2017
+{
+	public class Day19 : Puzzle
+	{
+		private readonly char[,] _diagram;
+		private int? _steps = null;
+
+		public Day19(string[] input) : base(input)
+		{
+			_diagram = input.To2DArray(input.Length, input[0].Length);
+		}
+
+		public override DateTime Date => new DateTime(2017, 12, 19);
+		public override string Title => "A Series of Tubes";
+
+		public override string? CalculateSolution()
+		{
+			Direction.SetMode(Direction.Mode.Array);
+			Direction direction = Direction.South;
+			Position pos = (0, _diagram.GetRow(0).IndexOf('|'));
+			string letters = string.Empty;
+			_steps = 0;
+
+			while (_diagram.TryGetValue(pos, out char c) && c != ' ')
+			{
+				if (c == '+')
+				{
+					direction = NewDirectionOrDefault();
+					if (direction == default)
+						break;
+				}
+				else if (char.IsLetter(c))
+					letters += c;
+				else if (c != '|' && c != '-')
+					break;
+
+				pos.Offset(direction, 1);
+				_steps++;
+			}
+
+			return Solution = letters;
+
+			Direction NewDirectionOrDefault()
+			{
+				Direction d = direction.ToRight();
+				if (_diagram.TryGetValue(pos + d, out char c) && c != ' ')
+					return d;
+
+				d = direction.ToLeft();
+				if (_diagram.TryGetValue(pos + d, out c) && c != ' ')
+					return d;
+
+				return default;
+			}
+		}
+
+		public override string? CalculateSolutionPartTwo()
+		{
+			if (!_steps.HasValue)
+				CalculateSolution();
+
+			return SolutionPartTwo = _steps.ToString();
+		}
+	}
+}
---
+++ tests/AdventOfCode.Tests/Year2017/Day19Tests.cs
@@ -0,0 +1,27 @@
+using AdventOfCode.Year2017;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Tests.Year2017
+{
+	[TestClass]
+	public class Day19Tests : PuzzleTests<Day19>
+	{
+		private static readonly string[] s_input = new[]
+		{
+			"     |          ",
+			"     |  +--+    ",
+			"     A  |  C    ",
+			" F---|----E|--+ ",
+			"     |  |  |  D ",
+			"     +B-+  +--+ "
+		};
+
+		[TestMethod]
+		[DataRow("ABCDEF")]
+		public void PartOneExamples(string expected) => CalculatePartOne(s_input, expected);
+
+		[TestMethod]
+		[DataRow("38")]
+		public void PartTwoExamples(string expected) => CalculatePartTwo(s_input, expected);
+	}
+}
