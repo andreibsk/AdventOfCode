5725f629bb7afdd57a667e6054917d1c5a964bd4
Andrei Bîscă <andreibsk+github@gmail.com>
2019-12-02 20:45:30 +0200

    Update gitignore and add template files to repo

---
+++ src/AdventOfCode/Year2019/Day00.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+using AdventOfCode.Common.Extensions;
+
+namespace AdventOfCode.Year2019
+{
+	public class Day00 : Puzzle
+	{
+		private readonly string[] _input;
+
+		public Day00(string[] input) : base(input)
+		{
+			_input = input;
+		}
+
+		public override DateTime Date => new DateTime(2019, 12, 00);
+		public override string Title => "";
+
+		public override string? CalculateSolution()
+		{
+			return Solution = "";
+		}
+
+		public override string? CalculateSolutionPartTwo()
+		{
+			return SolutionPartTwo = "";
+		}
+	}
+}
