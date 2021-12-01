7d469fec3754bdb88157a7876baa9df1790e9e98
Andrei Bîscă <andreibsk+github@gmail.com>
2021-12-01 15:33:04 +0200

    Add template files for 2020 and 2021

---
+++ src/AdventOfCode/Year2021/Day00.cs
@@ -0,0 +1,24 @@
+namespace AdventOfCode.Year2021;
+
+public class Day00 : Puzzle
+{
+	private readonly string[] _input;
+
+	public Day00(string[] input) : base(input)
+	{
+		_input = input;
+	}
+
+	public override DateTime Date => new DateTime(2021, 12, 00);
+	public override string Title => "";
+
+	public override string? CalculateSolution()
+	{
+		return Solution = "";
+	}
+
+	public override string? CalculateSolutionPartTwo()
+	{
+		return SolutionPartTwo = "";
+	}
+}
---
+++ tests/AdventOfCode.Tests/Year2020/Day00Tests.cs
@@ -0,0 +1,43 @@
+using AdventOfCode.Year2020;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Tests.Year2020;
+
+[TestClass]
+public class Day00Tests : PuzzleTests<Day00>
+{
+	private static readonly string[] s_input = new[]
+	{
+		""
+	};
+
+	[TestMethod]
+	[DataRow("", "")]
+	public void PartOneExamples(string input, string expected) => CalculatePartOne(input, expected);
+
+	[TestMethod]
+	[DataRow("")]
+	public void PartOneExamples(string expected) => CalculatePartOne(s_input, expected);
+
+	[TestMethod]
+	[DataRow(new[]
+	{
+		""
+	}, "")]
+	public void PartOneExamples(string[] input, string expected) => CalculatePartOne(input, expected);
+
+	[TestMethod]
+	[DataRow(new[]
+	{
+		""
+	}, "")]
+	public void PartTwoExamples(string[] input, string expected) => CalculatePartTwo(input, expected);
+
+	[TestMethod]
+	[DataRow("")]
+	public void PartTwoExamples(string expected) => CalculatePartTwo(s_input, expected);
+
+	[TestMethod]
+	[DataRow("", "")]
+	public void PartTwoExamples(string input, string expected) => CalculatePartTwo(input, expected);
+}
